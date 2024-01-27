5060fc7014e1537bdd43b36eebb54207fad659b4
eyhdev <alaqsa335@gmail.com>
2024-01-27 12:21:25 +0300

    Initial Commit

---
+++ MainView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MainView
+//
+//  Created by eyh.mac on 27.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MainView/MainViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewApp.swift
+//  MainView
+//
+//  Created by eyh.mac on 27.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MainViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
